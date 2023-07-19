fcb4a40d1ca2d33ba9cb9daea34361273d92f1a6
esmond-adjei <xmongram@gmail.com>
2023-07-19 18:38:41 +0000

    modified some files

--- hnh_accommodation/usermanagement/admin.py
+++ hnh_accommodation/usermanagement/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import HUser, HGuest, HManager
+from .models import HUser, HManager
 
 
 class ManagerPanel(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_verified')
+    list_display = ('username', 'email', 'get_managed_hostels')
+
+    def get_managed_hostels(self, obj):
+        return ', '.join([hostel.name for hostel in obj.hostels_managed.all()])
+
+    get_managed_hostels.short_description = 'Managed Hostels'
 
 
 admin.site.register(HUser)
